676525bc3904234ede056dc8eaa80b86cbd649b7
Mahreen Firdaws Rania <mahreenfirdaws2005@gmail.com>
2025-11-17 15:58:08 +0400

    Initial commit

---
+++ src/Product.java
@@ -0,0 +1,39 @@
+public class Product {
+    int productId;
+    String productName;
+    double price;
+    int quantity;
+
+    public int getProductId() {
+        return productId;
+    }
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+    public String getProductName() {
+        return productName;
+    }
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void displayProductInfo() {
+        System.out.println("Product ID: " + productId);
+        System.out.println("Product Name: " + productName);
+        System.out.println("Price: $" + price);
+        System.out.println("Quantity: " + quantity);
+    }
+    
+}
